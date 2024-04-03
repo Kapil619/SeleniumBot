05274efd6e75c0a01975e552a986289e891712ea
Kapil619 <kapilbadokar321@gmail.com>
2024-04-03 10:18:38 +0530

    Popout dismissal, Inserting search query & selecting checkin,checkout date.

--- booking/booking.py
+++ booking/booking.py
@@ -1,7 +1,8 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import booking.constants as const
-
 
 
 class Booking(webdriver.Chrome):
@@ -19,8 +20,35 @@ class Booking(webdriver.Chrome):
     def land_first_page(self):
         self.get(const.BASE_URL)
 
-    def change_currency(self,currency=None):
-        currency_element= self.find_element(by=By.XPATH, value='//*[@id="b2indexPage"]/div[2]/div/div/header/nav[1]/div[2]/span[1]/button')
+    def change_currency(self, currency=None):
+        currency_element = self.find_element(by=By.XPATH,
+                                             value='//*[@id="b2indexPage"]/div[2]/div/div/header/nav[1]/div[2]/span[1]/button')
         currency_element.click()
-        selected_currency_element = self.find_element(by=By.XPATH, value='//*[@id="b2indexPage"]/div[19]/div/div/div/div/div[2]/div/div[3]/div/div/div/ul[1]/li[1]/button')
+        selected_currency_element = self.find_element(by=By.XPATH,
+                                                      value='//*[@id="b2indexPage"]/div[19]/div/div/div/div/div[2]/div/div[3]/div/div/div/ul[1]/li[1]/button')
         selected_currency_element.click()
+
+    def check_popup(self):
+        try:
+            popup = self.find_element(by=By.XPATH,
+                                      value='//*[@id="b2indexPage"]/div[21]/div/div/div/div[1]/div[1]/div/button')
+            popup.click()
+            print('Popup dismissed')
+        except:
+            pass
+
+    def select_place_to_go(self, place_to_go):
+        search_field = self.find_element(by=By.XPATH, value='//*[@id=":re:"]')
+        search_field.clear()
+        self.check_popup()
+        search_field.send_keys(place_to_go)
+        search_field.click()
+        time.sleep(2)
+        first_result = self.find_element(by=By.XPATH, value='//*[@id="autocomplete-result-0"]')
+        first_result.click()
+
+    def select_dates(self, check_in_date, check_out_date):
+        check_in_element = self.find_element(by=By.CSS_SELECTOR, value=f'span[data-date="{check_in_date}"]')
+        check_in_element.click()
+        check_out_element = self.find_element(by=By.CSS_SELECTOR, value=f'span[data-date="{check_out_date}"]')
+        check_out_element.click()
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import time
+
 from booking.booking import Booking
 
 
 with Booking() as bot:
     bot.land_first_page()
-    bot.change_currency()
+    # bot.change_currency()
+    bot.select_place_to_go('New York')
+    bot.select_dates(check_in_date='2024-04-02', check_out_date='2024-04-05')
+    time.sleep(5)
+
+    input("Press Enter to exit...")
+
+
