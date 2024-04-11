182cd94fc283bbbc2ed4a73e2190abf544d3d6ef
Kapil619 <kapilbadokar321@gmail.com>
2024-04-11 09:49:00 +0530

    Added prettyTable package & configured the table for output

--- booking/booking.py
+++ booking/booking.py
@@ -4,7 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import booking.constants as const
 from booking.booking_filtration import BookingFiltration
-from booking.booking_report import BookingReport
+from prettytable import PrettyTable
+
 
 class Booking(webdriver.Chrome):
     def __init__(self, driver_path=r"C:\chromedriver.exe", teardown=False):
@@ -24,15 +25,13 @@ class Booking(webdriver.Chrome):
         self.get(const.BASE_URL)
 
     def change_currency(self, currency=None):
-        currency_element = self.find_element(by=By.XPATH,value='//*[@id="b2searchresultsPage"]/div[3]/div/div/header/nav[1]/div[2]/span[1]/button')
+        currency_element = self.find_element(by=By.XPATH,
+                                             value='//*[@id="b2searchresultsPage"]/div[3]/div/div/header/nav[1]/div[2]/span[1]/button')
         currency_element.click()
         selected_currency_element = self.find_element(by=By.XPATH,
                                                       value='//*[@id="b2indexPage"]/div[19]/div/div/div/div/div[2]/div/div[3]/div/div/div/ul[1]/li[1]/button')
         selected_currency_element.click()
 
-
-
-    
     def check_popup(self):
         try:
             print('Entered popup function')
@@ -40,7 +39,7 @@ class Booking(webdriver.Chrome):
             #                           value='//*[@id="b2indexPage"]/div[21]/div/div/div/div[1]/div[1]/div/button')
             popup = self.find_element(by=By.CSS_SELECTOR, value='button[aria-label="Dismiss sign-in info."]')
             print('Located element')
-           
+
             popup.click()
             print('Popup dismissed')
             return True
@@ -49,7 +48,7 @@ class Booking(webdriver.Chrome):
             pass
 
     def select_place_to_go(self, place_to_go):
-        try: 
+        try:
             print('Entered select_place_to_go function')
             search_field = self.find_element(by=By.XPATH, value='//*[@id=":re:"]')
             print('Located search field')
@@ -73,18 +72,20 @@ class Booking(webdriver.Chrome):
             print('Error in selecting dates: ', e)
 
     def select_adults(self, count=1):
-        try: 
+        try:
             selection_element = self.find_element(by=By.CSS_SELECTOR, value='button[data-testid="occupancy-config"]')
             selection_element.click()
             while True:
-                decrease_adult_element = self.find_element(by=By.XPATH, value='//*[@id=":rf:"]/div/div[1]/div[2]/button[1]')
+                decrease_adult_element = self.find_element(by=By.XPATH,
+                                                           value='//*[@id=":rf:"]/div/div[1]/div[2]/button[1]')
                 decrease_adult_element.click()
                 adults_value_element = self.find_element(by=By.ID, value='group_adults')
                 adults_value = adults_value_element.get_attribute('value')  # gets count for adults
                 if int(adults_value) == 1:
                     break
 
-            increase_button_element = self.find_element(by=By.XPATH, value='//*[@id=":rf:"]/div/div[1]/div[2]/button[2]')
+            increase_button_element = self.find_element(by=By.XPATH,
+                                                        value='//*[@id=":rf:"]/div/div[1]/div[2]/button[2]')
             for _ in range(count - 1):
                 increase_button_element.click()
         except Exception as e:
@@ -92,7 +93,7 @@ class Booking(webdriver.Chrome):
 
     def click_search(self):
         try:
-            
+
             search_button = self.find_element(by=By.CSS_SELECTOR, value='button[type="submit"]')
             search_button.click()
         except Exception as e:
@@ -104,19 +105,22 @@ class Booking(webdriver.Chrome):
         filtration.sort_price_lowest_first()
 
     def report_results(self):
-        hotels = self.find_element(by=By.CLASS_NAME, value='d4924c9e74').find_elements(by=By.CSS_SELECTOR, value='div[data-testid="property-card"]')
+        hotels = self.find_element(by=By.CLASS_NAME, value='d4924c9e74').find_elements(by=By.CSS_SELECTOR,
+                                                                                       value='div[data-testid="property-card"]')
         print(len(hotels))
-        hotel_data=[]
+        hotel_data = []
         for hotel in hotels:
-            name = hotel.find_element(by=By.CSS_SELECTOR, value='div[data-testid="title"]').get_attribute('innerHTML').strip()
+            name = hotel.find_element(by=By.CSS_SELECTOR, value='div[data-testid="title"]').get_attribute(
+                'innerHTML').strip()
             print(name)
-            hotel_price = hotel.find_element(by=By.CSS_SELECTOR, value='span[data-testid="price-and-discounted-price"]').get_attribute('innerHTML').strip()
+            hotel_price = hotel.find_element(by=By.CSS_SELECTOR,
+                                             value='span[data-testid="price-and-discounted-price"]').get_attribute(
+                'innerHTML').strip()
             hotel_price = hotel_price.replace('₹&nbsp;', '₹')  # remove '₹&nbsp;'
-            print(hotel_price)
-            hotel_score = hotel.find_element(by=By.CLASS_NAME, value='ac4a7896c7 ').get_attribute('innerHTML').strip()
-            hotel_score = hotel_score.replace('Scored ', '')  # remove 'Scored '
-            hotel_data.append({'name': name, 'price': hotel_price, 'score': hotel_score})
-        return hotel_data
-        
-        
-
+            # hotel_score = hotel.find_element(by=By.CLASS_NAME, value='ac4a7896c7 ').get_attribute('innerHTML').strip()
+            # hotel_score = hotel_score.replace('Scored ', '')  # remove 'Scored '
+            hotel_data.append((name, hotel_price))
+        table = PrettyTable(field_names=['Hotel Name', 'Hotel Price'])
+        table.add_rows(hotel_data)
+        print(table)
+        # return hotel_data
